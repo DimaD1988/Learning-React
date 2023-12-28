df4febb388f6ec4e7665553d7fe901ef0e947b05
DimaD1988 <dimdon88@gmail.com>
2023-12-28 22:58:37 +0300

    feat: add CodeSnippets useState5.js file

---
+++ React-Lessons/Exersices/02 Code Snipets/useState5.js
@@ -0,0 +1,11 @@
+// const inputChangeHandler = (identifier, value) => {
+// 	if (identifier === 'title') {
+// 		setEnteredTitle(value);
+// 	} else if (identifier === 'amount') {
+// 		setEnteredAmount(value);
+// 	} else {
+// 		setEnteredDate(value);
+// 	}
+// };
+
+// onChange={(e)=>inputChangeHandler('title',e.target.value)}
