76e730b787ee4579c3c0703a112b0932f3eab13b
DimaD1988 <dimdon88@gmail.com>
2024-02-23 17:00:19 +0300

    feat: add ContextAPI src folder

---
+++ React-Lessons/07 Context API/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import Product from './components/Product.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { CardContextProvider } from './store/shopping-card-context.jsx';
+
+function App() {
+	return (
+		<CardContextProvider>
+			<Header />
+			<Shop>
+				{DUMMY_PRODUCTS.map((product) => (
+					<li key={product.id}>
+						<Product {...product} />
+					</li>
+				))}
+			</Shop>
+		</CardContextProvider>
+	);
+}
+
+export default App;
